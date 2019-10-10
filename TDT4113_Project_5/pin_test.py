e821f279d837d806cf6d828e0971c74a5f08cc05
henrfj <h1rik.f@gmail.com>
2019-10-10 14:48:11 +0200

    made the test easier to repeat

--- TDT4113_Project_5/pin_test.py
+++ TDT4113_Project_5/pin_test.py
@@ -7,10 +7,12 @@ GPIO.setwarnings(False)
 test_pin = 0
 while test_pin != -1:
     test_pin = int(input("Enter what pin will you test: "))
+    print("You entered pin nr. :", test_pin)
     GPIO.setup(test_pin,GPIO.OUT)
     print ("LED on")
     GPIO.output(test_pin,GPIO.HIGH)
-    time.sleep(1)
+    input ("press enter to turn off")
     print ("LED off")
     GPIO.output(test_pin,GPIO.LOW)
+    time.sleep(1)
 
