2a3badfba29e38f0f6615a4176b2ba8d98b44fae
henrfj <h1rik.f@gmail.com>
2019-10-10 14:43:30 +0200

    made the test more general

--- TDT4113_Project_5/pin_test.py
+++ TDT4113_Project_5/pin_test.py
@@ -1,14 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-
-test_pin = 25
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setwarnings(False)
-GPIO.setup(test_pin,GPIO.OUT)
-print ("LED on")
-GPIO.output(test_pin,GPIO.HIGH)
-time.sleep(1)
-print ("LED off")
-GPIO.output(test_pin,GPIO.LOW)
+
+test_pin = 0
+while test_pin != -1:
+    test_pin = int(input("Enter what pin will you test: "))
+    GPIO.setup(test_pin,GPIO.OUT)
+    print ("LED on")
+    GPIO.output(test_pin,GPIO.HIGH)
+    time.sleep(1)
+    print ("LED off")
+    GPIO.output(test_pin,GPIO.LOW)
+
