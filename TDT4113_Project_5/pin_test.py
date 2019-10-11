199df581c2d7ccb64ab720378b8d030c74797972
henrfj <h1rik.f@gmail.com>
2019-10-11 11:20:54 +0200

    mf

--- TDT4113_Project_5/pin_test.py
+++ TDT4113_Project_5/pin_test.py
@@ -7,14 +7,15 @@ GPIO.setwarnings(False)
 test_pin = 0
 while test_pin != -1:
     test_pin = int(input("Enter what pin will you test: -1 for Charlietest"))
-    print("You entered pin nr. :", test_pin)
-    GPIO.setup(test_pin,GPIO.OUT)
-    print ("LED on")
-    GPIO.output(test_pin,GPIO.HIGH)
-    input ("press enter to turn off")
-    print ("LED off")
-    GPIO.output(test_pin,GPIO.LOW)
-    time.sleep(1)
+    if test_pin != -1:
+        print("You entered pin nr. :", test_pin)
+        GPIO.setup(test_pin,GPIO.OUT)
+        print ("LED on")
+        GPIO.output(test_pin,GPIO.HIGH)
+        input ("press enter to turn off")
+        print ("LED off")
+        GPIO.output(test_pin,GPIO.LOW)
+        time.sleep(1)
 
 
 print("Charlietest!")
